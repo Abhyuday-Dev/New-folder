b03a3ae32db40e0eaeb38ac910ace53addca32b6
Abhyuday-Dev <abhyuday7176@hotmail.com>
2024-06-06 22:54:48 +0530

    first commit

---
+++ Client/src/Pages/login/index.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+
+interface ILoginProps {}
+
+const Login: React.FunctionComponent<ILoginProps> = (props) => {
+  return <div>Error</div>;
+};
+
+export default Login;
---
+++ Client/src/routes.tsx
@@ -0,0 +1,49 @@
+import { createBrowserRouter } from "react-router-dom";
+import Login from "./Pages/login";
+import Error from "./Pages/error";
+import Signup from "./Pages/signup";
+import Home from "./Pages/home";
+import Profile from "./Pages/profile";
+import Post from "./Pages/Post";
+import MyPhotos from "./Pages/myPhotos";
+import ProtectedRoutes from "./components/ProtectedRoute";
+
+export const router = createBrowserRouter([
+  {
+    element: <ProtectedRoutes />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+        errorElement: <Error />,
+      },
+      {
+        path: "/profile",
+        element: <Profile />,
+        errorElement: <Error />,
+      },
+      {
+        path: "/post",
+        element: <Post />,
+        errorElement: <Error />,
+      },
+      {
+        path: "/myPhotos",
+        element: <MyPhotos />,
+        errorElement: <Error />,
+      },
+    ],
+  },
+  {
+    path: "/login",
+    element: <Login />,
+    errorElement: <Error />,
+  },
+  {
+    path: "/signup",
+    element: <Signup />,
+    errorElement: <Error />,
+  },
+]);
+
+export default router;
